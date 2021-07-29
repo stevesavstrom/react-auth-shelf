7216533a7d1885a485e14b9b177c962e8b2d8405
Matthew Bouc <boucgm@gmail.com>
2021-07-29 17:20:06 -0500

    AddItem page created and filled in

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -4,36 +4,36 @@ import { useDispatch } from 'react-redux';
 function AddItem () {
 
     const dispatch = useDispatch();
-    const [newItem, setNewItem] = useState('');
-    
+    const [newDescription, setNewDescription] = useState('');
+    const [newImage, setNewImage] = useState("");
 
-    //Need dispatch to POST saga
-
-    const addMovieData = () => {
-        event.preventDefault();
-        const newItem =
-        {
-            description: description
-            image_url
-        }
-        dispatch({ type: 'ADD_MOVIE', payload: newMovie })
-        setDescription('')
-        setTitle('')
-        setURL('')
-        history.push('/')
+    const addNewItem = () => {
+      event.preventDefault();
+      const newItem = {
+        description: newDescription,
+        image_url: newImage,
+      };
+      //Need dispatch to POST saga
+      dispatch({ type: "ADD_ITEM", payload: newItem });
     }
     //Form to add items to itemsDB
     return (
       <section>
-        <form>
+        <form action="submit">
           <input
             type="text"
             value={newItem}
-            placeholder="Add Item"
-            onChange={(evt) => setNewItem(evt.target.value)}
-            required
-          />
+            placeholder="Item Description"
+            onChange={(evt) => setNewDescription(evt.target.value)}
+            required/>
+          <input
+            type="text"
+            value={newImage}
+            placeholder="Image URL"
+                    onChange={(evt) => setNewImage(evt.target.value)}
+            required />
         </form>
+            <button type='submit' onClick={() => addNewItem()}>Add Item</button>
       </section>
     );
 }
